da2d8f157886e3030aa5edf17e7e2ab562195371
Martin <mts13@hotmail.fr>
2018-10-16 11:55:24 +0200

    Fonctionnel ! Alpha v0.2

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -1,3 +1,5 @@
+require 'colorize'
+
 class Player
   attr_accessor :firstname
 
@@ -27,15 +29,17 @@ class Board
 end
 
 class BoardCase
-  attr_accessor :tab, :chosen_case, :symbol
+  attr_accessor :tab, :chosen_case, :symbol, :count
 
-  def initialize(tab, chosen_case, symbol)
+  def initialize(tab, chosen_case, symbol, count)
     @tab = tab
     @chosen_case = chosen_case
     @symbol = symbol
+    @count = count
   end
 
   def board_update
+    system "clear"
     if @tab[@chosen_case - 1] == " "
       @tab[@chosen_case - 1] = @symbol
       x = 0
@@ -54,33 +58,39 @@ class BoardCase
           y += 1
       end
       print "\n"
-      return @tab
+      return @tab, @count
     else
       puts "Case occupée !"
-      sleep(2)
+      sleep(1)
+      @count -= 1
+      return @tab, @count
     end
   end
 end
 
 class Verification
-  attr_accessor :tab
+  attr_accessor :tab, :count
 
-  def initialize(tab)
+  def initialize(tab, count)
     @tab = tab
+    @count = count
   end
 
   def is_finished
-  	joueur = 0
-  	if tab[0] == "x" && tab[1] == "x" && tab[2] == "x" || tab[3] == "x" && tab[4] == "x" && tab[5] == "x" || tab[6] == "x" && tab[7] == "x" && tab[8] == "x" || tab[0] == "x" && tab[3] == "x" && tab[6] == "x" || tab[1] == "x" && tab[4] == "x" && tab[7] == "x" || tab[2] == "x" && tab[5] == "x" && tab[8] == "x" || tab[0] == "x" && tab[4] == "x" && tab[8] == "x" || tab[2] == "x" && tab[4] == "x" && tab[6] == "x"
-  	  return joueur = 1
-  	elsif tab[0] == "o" && tab[1] == "o" && tab[2] == "o" || tab[3] == "o" && tab[4] == "o" && tab[5] == "o" || tab[6] == "o" && tab[7] == "o" && tab[8] == "o" || tab[0] == "o" && tab[3] == "o" && tab[6] == "o" || tab[1] == "o" && tab[4] == "o" && tab[7] == "o" || tab[2] == "o" && tab[5] == "o" && tab[8] == "o" || tab[0] == "o" && tab[4] == "o" && tab[8] == "o" || tab[2] == "o" && tab[4] == "o" && tab[6] == "o"
-  	  return joueur = 2
-  	else
-  		return joueur
-  	end
+    joueur = 0
+    if tab[0] == "x".red && tab[1] == "x".red && tab[2] == "x".red || tab[3] == "x".red && tab[4] == "x".red && tab[5] == "x".red || tab[6] == "x".red && tab[7] == "x".red && tab[8] == "x".red || tab[0] == "x".red && tab[3] == "x".red && tab[6] == "x".red || tab[1] == "x".red && tab[4] == "x".red && tab[7] == "x".red || tab[2] == "x".red && tab[5] == "x".red && tab[8] == "x".red || tab[0] == "x".red && tab[4] == "x".red && tab[8] == "x".red || tab[2] == "x".red && tab[4] == "x".red && tab[6] == "x".red
+      return joueur = 1
+    elsif tab[0] == "o".blue && tab[1] == "o".blue && tab[2] == "o".blue || tab[3] == "o".blue && tab[4] == "o".blue && tab[5] == "o".blue || tab[6] == "o".blue && tab[7] == "o".blue && tab[8] == "o".blue || tab[0] == "o".blue && tab[3] == "o".blue && tab[6] == "o".blue || tab[1] == "o".blue && tab[4] == "o".blue && tab[7] == "o".blue || tab[2] == "o".blue && tab[5] == "o".blue && tab[8] == "o".blue || tab[0] == "o".blue && tab[4] == "o".blue && tab[8] == "o".blue || tab[2] == "o".blue && tab[4] == "o".blue && tab[6] == "o".blue
+      return joueur = 2
+    elsif count == 8
+      return joueur = 3
+    else
+      return joueur
+    end
   end
 end
 
+system "clear"
 
 puts "Nom du 1er joueur :"
 player1 = Player.new(gets.chomp)
@@ -98,7 +108,7 @@ count = 0
 help = true
 verif = true
 
-while verif == true || count <= 9
+while verif == true && count <= 9
   if help == true
     puts "/!\\ Attention /!\\ \nVoici une aide pour le choix des cases :\n1 | 2 | 3\n---------\n4 | 5 | 6\n---------\n7 | 8 | 9"
     help = false
@@ -106,21 +116,24 @@ while verif == true || count <= 9
   puts "Choisir une case entre 1 et 9 :"
   choice = gets.chomp.to_i
   if count % 2 == 0 && count != 1
-    game = BoardCase.new(game, choice, "x")
-    game = game.board_update
+    game, count = BoardCase.new(game, choice, "x".red, count)
+    game, count = game.board_update
   else
-    game = BoardCase.new(game, choice, "o")
-    game = game.board_update
+    game, count = BoardCase.new(game, choice, "o".blue, count)
+    game, count = game.board_update
   end
-  verif = Verification.new(game)
-  verif = verif.is_finished
-  if verif == 0
+  win = Verification.new(game, count)
+  win = win.is_finished
+  if win == 0
   	count += 1
-  elsif verif == 1
+  elsif win == 1
   	puts "#{player1} à gagné !"
   	verif = false
-  else verif == 2
+  elsif win == 2
   	puts "#{player2} à gagné !"
   	verif = false
+  else
+    puts "Égalité !"
+	verif = false
   end
 end
